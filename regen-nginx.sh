ea96851b4edf0b4b82c4ba0ff14fa2ea79de1c57
Daisy Ossel <daisyossel@gmail.com>
2026-05-24 20:25:48 +0200

    Fix regen-nginx.sh to use correct fluxer_app port 40000
    - Changed from 49319 to 40000 to match actual docker container
    - fluxer_app is listening on 40000:8080

--- regen-nginx.sh
+++ regen-nginx.sh
@@ -2,4 +2,4 @@
 # Regenerate nginx configs for proxcord.cc
 sudo rm /etc/nginx/sites-enabled/*.conf
 sudo rm /etc/nginx/sites-available/*.conf
-./nginx-config-only.sh proxcord.cc --fluxer-public-port 49319 --fluxer-admin-port 9899
+./nginx-config-only.sh proxcord.cc --fluxer-public-port 40000 --fluxer-admin-port 9899
