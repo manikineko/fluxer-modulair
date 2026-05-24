31114b8a90d94ac9ed84ff8721b6250f50624dc5
Daisy Ossel <daisyossel@gmail.com>
2026-05-24 20:51:37 +0200

    a

--- regen-nginx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Regenerate nginx configs for proxcord.cc
-sudo rm /etc/nginx/sites-enabled/*.conf
-sudo rm /etc/nginx/sites-available/*.conf
-./nginx-config-only.sh proxcord.cc --fluxer-public-port 49319 --fluxer-admin-port 9899
